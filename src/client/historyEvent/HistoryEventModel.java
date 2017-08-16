457a0e0c3fffde4406a508d3dab5acbe4d9a8b69
Blazej Gdaniec <bakogda@gmail.com>
2017-08-16 07:11:47 +0100

    re #34 Removed

--- src/client/historyEvent/HistoryEventModel.java
+++
@@ -1,8 +0,0 @@
-package client.historyEvent;
-
-public class HistoryEventModel {
-
-		public HistoryEventModel(){
-			System.out.println("View Event History: ");
-		}
-}
--- src/clients/currentShares/CurrentSharesController.java
+++
@@ -1,5 +0,0 @@
-package clients.currentShares;
-
-public class CurrentSharesController {
-
-}
